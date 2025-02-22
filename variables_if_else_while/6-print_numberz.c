e34290ad75a1c61c7556f7b6f18359e5791e51e8
Wrennie2004 <10423@holbertonstudents.com>
2025-02-22 02:22:16 +0000

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+	i = 48;
+        
+	while (i <= 57)
+        {
+                putchar(i);
+                i = i + 1;
+        }
+        putchar('\n');
+
+    return (0);
+}
