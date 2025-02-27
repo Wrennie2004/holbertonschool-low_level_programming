bf8d13fdad070568def033fd0d17f89049fd4c4d
Wrennie2004 <10423@holbertonstudents.com>
2025-02-27 01:31:59 +0000

    1

---
+++ functions_nested_loops/1-aplabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * Description: function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch = ch + 1;
+	}
+	_putchar('\n');
+}
