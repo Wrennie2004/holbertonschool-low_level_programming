fa3e64c787e8a505b251b4172c349c28a7472b47
Wrennie2004 <10423@holbertonstudents.com>
2025-02-27 05:05:14 +0000

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * Write a function that prints the sign of a number.
+ *
+ *int print_sign(int n) - prints a sign depending on the value of n.
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a);
+        return(a);
+}
+
