1befc15181f97399af664df957f1f8bf714c293e
Wrennie2004 <10423@holbertonstudents.com>
2025-02-27 01:36:41 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
-	/**
- 	* main - Entry point.
- 	* Description: function that prints the alphabet, in lowercase, followed by a new line.
-	* Return: Always 0 (Success)
-	*/
+/**
+ * main - Entry point.
+ * Description: Write a function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (Success.)
+ */ 
 void print_alphabet(void)
 {
 	char ch = 'a';
