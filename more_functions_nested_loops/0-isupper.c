c15ba488d01e3af31003627fcb64d3c55a95cbbd
Wrennie2004 <10423@holbertonstudents.com>
2025-03-02 23:42:39 +0000

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int _isupper(int c) - a function that checks for uppercase character.
+ *
+ * return(0) - success
+ */
+
+int _isupper(int c)
+{
+	return(c >= 'A' && c <= 'Z');
+}
