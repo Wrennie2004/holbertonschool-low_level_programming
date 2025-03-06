91aa7297174f165802f3f9514a4da172b46085ea
Wrennie2004 <10423@holbertonstudents.com>
2025-03-06 04:00:59 +0000

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@ void puts2(char *str)
         while(str[count] != '\0')
         {
                 _putchar(str[count]);
-                count = count + 2;
+                count +=  2;
         }
         _putchar('\n');
 }
