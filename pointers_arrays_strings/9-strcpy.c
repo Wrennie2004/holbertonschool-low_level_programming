d3e1fb390e3d77a48522623bfa14afa9f0413f89
Wrennie2004 <10423@holbertonstudents.com>
2025-03-07 02:51:01 +0000

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while(src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count = count + 1;
+	}
+
+	dest[count] = '\0';
+
+	return(dest);
+}
