8f50fda7538cceb56228661474852869d44d933c
Wrennie2004 <10423@holbertonstudents.com>
2025-06-26 06:27:09 +0000

    alphabaet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: a function that prints 10 times the alphabet
+ * in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+	
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c = c + 1;
+		}
+	i = i + 1;
+	_putchar('\n');
+	}
+}
