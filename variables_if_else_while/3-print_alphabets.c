3a5df25ec33ee98d20eff3e80d7b073b5b37be0a
Wrennie2004 <10423@holbertonstudents.com>
2025-06-26 05:19:30 +0000

    betty

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Description:a program that prints the alphabet in lowercase
+ *and then in uppercase, followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+
 
 int main(void)
 {
