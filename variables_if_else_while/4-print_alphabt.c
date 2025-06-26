541a51f9d397ffc7bc0450f1c4075adb445c2b49
Wrennie2004 <10423@holbertonstudents.com>
2025-06-26 05:21:56 +0000

    betty

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *
+ *Description: Write a program that prints the alphabet in lowercase
+ *followed by a new line.
+ *
+ *Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 	char c;
+
 	c = 'a';
 
 	while (c <= 'z')
 	{
-		if (c != 'q' && c != 'e')	
+		if (c != 'q' && c != 'e')
 		putchar(c);
 		c = c + 1;
-		
 	}
 	putchar('\n');
 
