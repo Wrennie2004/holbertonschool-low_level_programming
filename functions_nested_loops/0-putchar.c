b4cda1a66366d9e79dd16bbb630891be1851640d
Wrennie2004 <10423@holbertonstudents.com>
2025-06-26 05:49:26 +0000

    p u t c h a r

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	
+	return (0);
+}
