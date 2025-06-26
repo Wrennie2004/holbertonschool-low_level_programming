6342710e97eb4145099099c40647cca0951f4d69
Wrennie2004 <10423@holbertonstudents.com>
2025-06-26 05:26:00 +0000

    betty

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *
+ *Description:a program that prints all single digit
+ *numbers of base 10 starting from 0, followed by a new line.
+ *
+ *Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 	char num;
+
 	num = '0';
 
 	while (num <= '9')
--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: a program that prints all single digit
+ *numbers of base 10 starting from 0, followed by a new line.
+ *
+ *Return: Always 0 (Sucess)
+ */
+
 
 int main(void)
 {
 	int i;
+
 	i  = 48;
 
 	while (i <= 57)
