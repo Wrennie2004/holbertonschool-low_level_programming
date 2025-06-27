2dd00040e62276b41df67bfd7f1c0ee0193c3815
Wrennie2004 <10423@holbertonstudents.com>
2025-06-27 02:57:05 +0000

    bety

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,6 +1,9 @@
 #include "main.h"
-
+/**
+ * Description: Write a function that checks for lowercase character. 
+ * Return: Always 0 (Sucess)
+ */
 int _isalpha(int c)
 {
-	return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
