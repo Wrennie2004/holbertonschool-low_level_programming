66c10748f916e457e5419515551718dfbcffcb7a
Wrennie2004 <10423@holbertonstudents.com>
2025-06-29 02:22:40 +0000

    betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_numbers - Prints the numbers from 0 to 9 followed by a new line
+ *
+ * Return: void
+ */
 void print_numbers(void)
 {
 	int n;
