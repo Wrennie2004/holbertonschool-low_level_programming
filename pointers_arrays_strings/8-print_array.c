868c937316597c00a4584dc16aa77c5a14cd41e0
Wrennie2004 <10423@holbertonstudents.com>
2025-07-04 06:13:10 +0000

    weeoo

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int count;
+	
+	count = 0;
+	while (count < n)
+	{
+		printf("%d", a[count]);
+
+		if (count < n - 1)
+		{
+			printf(", ");
+		}
+		count = count + 1;
+	}
+
+	printf('\n');
+}
+
