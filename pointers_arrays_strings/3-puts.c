dae5f0686e9c4e88d276decd86f8317a2360e345
Wrennie2004 <10423@holbertonstudents.com>
2025-07-04 16:40:52 +1000

    Update 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _puts - Prints a string followed by a newline
+ * @str: Pointer to the string to be printed
+ *
+ * Description: This function prints each character of the string
+ *              pointed to by `str` using the `_putchar` function,
+ *              followed by a newline.
+ */
 void _puts(char *str)
 {
 	int count;
