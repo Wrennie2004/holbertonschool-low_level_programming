51533b57ee0d6d0d7dd9a4c9161bdeba30a2e2b9
Wrennie2004 <10423@holbertonstudents.com>
2025-07-04 06:33:23 +0000

    weeeee

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string into an integer
+ *
+ * @s: Input string
+ * Description:
+ * Return: Int
+ */
+
+int _atoi(char *s)
+{
+	int count = 0;
+	int sign = 1;
+	int start_num = 0;
+	int end_num = 0;
+	int result = 0;
+
+	while (s[count] != '\0')
+	{
+		if (s[count] == '-' && start_num == 0)
+		{
+			sign = sign * -1;
+		}
+
+		if (s[count] >= '0' && s[count] <= '9' && end_num == 0)
+		{
+			start_num = 1;
+			result = (result * 10) + (s[count] - '0') * sign;
+		}
+		else if (!(s[count] >= '0' && s[count] <= '9') && start_num == 1)
+		{
+			end_num = 1;
+		}
+
+		n++;
+	}
+	return (result);
+}
