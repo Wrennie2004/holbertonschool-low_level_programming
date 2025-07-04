704e17f41f0cd1693799fb90d91115600f82de8f
Wrennie2004 <10423@holbertonstudents.com>
2025-07-04 16:36:50 +1000

    Update 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,10 @@
+/**
+ * reset_to_98 - Updates the value a pointer points to to 98
+ * @n: Pointer to an integer to be updated
+ *
+ * Description: This function takes a pointer to an integer and
+ *              changes the value stored at that address to 98.
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
