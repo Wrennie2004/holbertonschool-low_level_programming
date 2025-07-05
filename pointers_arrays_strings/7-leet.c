3c0a3cd2d260e3d4d55fa3af6b1eab56d0650f0e
Wrennie2004 <10423@holbertonstudents.com>
2025-07-05 13:09:04 +1000

    Update 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,5 +1,15 @@
 #include "main.h"
-
+/**
+ * leet - Encodes a string into 1337 (leet) speak.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the resulting string `str`.
+ *
+ * Description: This function replaces certain letters in the input string
+ * with corresponding leet speak digits. Specifically, it converts
+ * 'a' or 'A' to '4', 'e' or 'E' to '3', 'o' or 'O' to '0', 't' or 'T' to '7',
+ * and 'l' or 'L' to '1'.
+ */
 char *leet(char *str)
 {
 	int n = 0, i = 0; /* Pointers */
