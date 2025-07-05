e2801ff2dcc13c4b4be4208328a30bf3ffbf3f26
Wrennie2004 <10423@holbertonstudents.com>
2025-07-05 03:03:50 +0000

    weeooo

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *leet(char *str)
+{
+	int n = 0, i = 0; /* Pointers */
+	char alpha[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
+
+	while (str[n] != '\0')
+	{
+		i = 0;
+		while (alpha[i] != '\0')
+		{
+			if (str[n] == alpha[i])
+			{
+				str[n] = leet[i];
+			}
+			i++;
+		}
+		n++;
+	}
+	return (str);
+}
